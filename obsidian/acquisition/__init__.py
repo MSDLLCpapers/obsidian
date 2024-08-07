22bae69805db6deac29e7e426af33fc67213b4dd
Kevin Stone <kevin.stone38@gmail.com>
2024-08-07 11:23:24 -0400

    Standardization of __init__ files

--- obsidian/acquisition/__init__.py
+++ obsidian/acquisition/__init__.py
@@ -1,15 +1,4 @@
 """Acquisition: Functions to determine the value of sequential experiments"""
 
-from .acquisition_botorch import (
-    qMean,
-    qSpaceFill,
-    qExpectedImprovement,
-    qProbabilityOfImprovement,
-    qUpperConfidenceBound,
-    qNoisyExpectedImprovement,
-    qSimpleRegret,
-    qNoisyExpectedHypervolumeImprovement,
-    qLogNParEGO,
-)
-
-from .aq_config import aq_functions, valid_aqs, aq_hp_defaults
+from .acquisition_botorch import *
+from .aq_config import *
